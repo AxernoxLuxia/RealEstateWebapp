7c46d87b34ddc9a82546a8e9504923296a19a0ec
AxernoxLuxia <agarwalaryan749@gmail.com>
2024-08-15 11:07:46 +0530

    Completed user update functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -21,7 +21,7 @@ export const updateUser = async (req, res, next) => {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password,
-          avatarUrl: req.body.avatar,
+          avatar: req.body.avatar,
         },
       },
       { new: true }
