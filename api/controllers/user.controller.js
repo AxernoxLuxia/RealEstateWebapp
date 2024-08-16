2d1b4c738304d220a885f779ce84923e8a1def8d
AxernoxLuxia <agarwalaryan749@gmail.com>
2024-08-16 21:32:53 +0530

    Created listing adding func

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -46,3 +46,16 @@ export const deleteUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  } else {
+    try {
+      const listings = await Listing.find({userRef: req.params.id});
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  }
+};
