e708aeb4f58e0ba7ad362e035628c81b3a7126bd
AxernoxLuxia <agarwalaryan749@gmail.com>
2024-08-18 01:33:49 +0530

    Created listing item component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -110,7 +110,7 @@ export default function Listing() {
                                 <p className="bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md">
                                     ${" "}
                                     {+listing.regularPrice -
-                                        +listing.discountPrice}
+                                        +listing.discountPrice} OFF
                                     {listing.type === "rent" && " / month"}
                                 </p>
                             )}
