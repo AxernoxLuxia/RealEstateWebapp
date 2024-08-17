55da7480c2da26297a5ea5586b4320769590e572
AxernoxLuxia <agarwalaryan749@gmail.com>
2024-08-18 03:01:49 +0530

    Completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,11 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About RealEstate</h1>
+      <p className="mb-4 text-slate-700">p1</p>
+      <p className="mb-4 text-slate-700">p2</p>
+    </div>
   )
 }
 
